cd8dc2f02f8a63b1bdebb2a8b003114f230e8b26
Abdellahsyani <syaniabdos@gmail.com>
2024-03-22 15:31:36 +0000

    amenities method

--- models/place.py
+++ models/place.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+
+place_amenity = Table('place_amenity', Base.metadata,
+        Column('place_id', String(60),
+            ForeignKey('places.id'),
+            primary_key=True, nullable=False),
+        Column('amenity_id', String(60),
+            ForeignKey('amenities.id'),
+            primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -18,10 +26,14 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    # amenity_ids = []
+    amenity_ids = []
     
     # this is for DBStorage
     reviews = relationship('Review', cascade='all, delete', backref='place')
+    amenities = relationship('Amenity', cascad='all, delete',
+                            backref='place_amenity',
+                            secondary=place_amenity,
+                            viewonly=False)
 
     #this is for FileStorage
     @property
@@ -33,3 +45,13 @@ class Place(BaseModel, Base):
                 review_list.append(rev)
 
         return (rev_list)
+
+    @property
+    def amenities(self):
+        """method to return all list of Amenity instance"""
+        pass
+
+    @amenities.setter
+    def amenities(self):
+        """setter method to handle append method"""
+        pass
