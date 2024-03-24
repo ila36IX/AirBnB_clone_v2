6e2f11858c5cce5dc6261c28e3021040fb615053
ila36IX <jbariali002@gmail.com>
2024-03-24 08:44:51 +0000

    changing the output of console

--- models/__init__.py
+++ models/__init__.py
@@ -8,7 +8,6 @@ if getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
-    print("testin is everything")
     storage = FileStorage()
 
 storage.reload()
