7eafeab80b3ed502b4f1d5f639f46994d069ebd9
ila36IX <jbariali002@gmail.com>
2024-04-21 01:57:41 +0100

    Prepare the web servers for CI

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,12 +8,16 @@ mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "Everything is set up perfectly" > /data/web_static/releases/test/index.html
 
-ln -sf /data/web_static/current /data/web_static/releases/test/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
 # Give the ubuntu user the ownership of /data/
 chown ubuntu:ubuntu -R /data
 
 # Update the Nginx configuration to serve the content of
 # /data/web_static/current/ to hbnb_static.
-hbnb_static_alias="location \/hbnb_static\/ \{\nalias \/data\/web_static\/current\/\;autoindex off\;\n\}"
-sed -i "/root/a $hbnb_static_alias" /etc/nginx/sites-available/getalien.tech
+if [ "$(grep -c "location /hbnb_static" /etc/nginx/sites-available/getalien.tech)" -ne 1 ]
+then
+	hbnb_static_alias="location \/hbnb_static \{\nalias \/data\/web_static\/current\/\;autoindex off\;\n\}"
+	sed -i "/root/a $hbnb_static_alias" /etc/nginx/sites-available/getalien.tech
+fi
 nginx -s reload
