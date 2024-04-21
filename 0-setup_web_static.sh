612f7466b313661a5c4c78ee1fd79a45c8bab1e2
ila36IX <jbariali002@gmail.com>
2024-04-21 03:47:15 +0100

    test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,12 +3,8 @@
 sudo apt-get update
 sudo apt-get -y install nginx
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
 sudo echo "All set up perfectly" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
