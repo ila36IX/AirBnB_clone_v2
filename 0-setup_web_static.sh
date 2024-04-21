e5324d04e6652c4d0467c76b681a694395722dda
ila36IX <jbariali002@gmail.com>
2024-04-21 02:04:08 +0100

    Prepare the web servers for CI

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 # Sets up the web servers for the deployment of web_static
 # Install nginx
-apt-get install nginx -y
+sudo apt-get update -y
+sudo apt-get install nginx -y
 
 # Create the essentails directories
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo "Everything is set up perfectly" > /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo echo "Everything is set up perfectly" > /data/web_static/releases/test/index.html
 
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give the ubuntu user the ownership of /data/
-chown ubuntu:ubuntu -R /data
+sudo chown ubuntu:ubuntu -R /data
 
 # Update the Nginx configuration to serve the content of
 # /data/web_static/current/ to hbnb_static.
 if [ "$(grep -c "location /hbnb_static" /etc/nginx/sites-available/getalien.tech)" -ne 1 ]
 then
 	hbnb_static_alias="location \/hbnb_static \{\nalias \/data\/web_static\/current\/\;autoindex off\;\n\}"
-	sed -i "/root/a $hbnb_static_alias" /etc/nginx/sites-available/getalien.tech
+	sudo sed -i "/root/a $hbnb_static_alias" /etc/nginx/sites-available/getalien.tech
 fi
-nginx -s reload
-exit 0
+sudo nginx -s reload
